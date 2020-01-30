0eb8add890d29e65f1a0cc6c2e565facb04e77eb
Chang-suk <jcs191072@gmail.com>
2020-01-31 00:02:33 +0900

    packages

---
+++ vue-news/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <!-- url 주소에 따라서 router-view가 변경-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue';
+
+export default {
+ components: {
+   ToolBar,
+ },
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
