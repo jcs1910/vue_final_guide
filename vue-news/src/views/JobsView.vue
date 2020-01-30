9545a088e59fb5dea920a1fd32798ff3ca19f39b
Chang-suk <jcs191072@gmail.com>
2020-01-31 02:16:07 +0900

    API 구현

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    Jobs
+    <div v-for="job in jobs">{{ job.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchJobList } from "../api/index.js"
 
+export default {
+  data() {
+    return {
+      jobs: []
+    }
+  },
+  created() {
+    fetchJobList()
+      .then(response => this.jobs = response.data)
+      .catch(error => console.log(error));
+  }
 }
 </script>
 
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    News
+    <div v-for="user in users">{{ user.title}}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchNewsList } from '../api/index.js';
 
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created(){
+    fetchNewsList()
+      .then(response => {
+        console.log(response)
+        this.users = response.data;
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+  }
 }
 </script>
 
